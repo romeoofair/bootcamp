b4b6e320e4e1a70741f3d961afde1022f9346dec
bharath kumar <bharathaero225@gmail.com>
2017-11-29 21:19:36 +0530

    patatap

--- 5.JQuery/assets/js/todos.js
+++ 5.JQuery/assets/js/todos.js
@@ -1,5 +1,5 @@
 //Check off specific todos by Clicking
- $("li").click(function(){ 
+ $("ul").on("click","li", function(){ 
     // //if li is gray
     // if($(this).css("color") === "rgb(128, 128, 128)") {
     //   //turn it black
@@ -20,7 +20,7 @@
   });
 
 //click on x to delete todo
-  $("span").click(function(e){
+  $("ul").on("click", "span", function(e){
     $(this).parent().fadeOut(500,function(){
       $(this).remove();
     });
@@ -32,6 +32,10 @@
       var todoText = $(this).val();
       $(this).val("");
       //create a new li and add to ul
-      $("ul").append("<li><span>X</span> "+ todoText +"</li>");
+      $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> "+ todoText +"</li>");
     }
-  });
+  }); 
+
+  $(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+  })
